c21748712584db0debbaa80ec9630bc8dc219ba5
Nelson Nobre <technobre@gmail.com>
2024-07-11 00:38:58 +0100

    Improved demo

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 def insert_sort(arr): # O(n^2)
-    for i in range(1, len(arr)): # n -> O(n)
-        curr = arr[i] # 1 * n -> O(n)
-        j = i - 1 # 1 * n -> O(n)
+    n = len(arr) # O(1)
 
-        while j >= 0 and arr[j] > curr: # n * n -> O(n^2)
-            arr[j + 1] = arr[j] # 1 * n * n -> O(n^2)
+    for i in range(1, n): # n -> O(n)
+        curr = arr[i] # 1 * n -> O(n)
+        j = i # 1 * n -> O(n)
+
+        while j > 0 and arr[j - 1] > curr: # n * n -> O(n^2)
+            arr[j] = arr[j - 1] # 1 * n * n -> O(n^2)
             j -= 1 # 1 * n * n -> O(n^2)
 
-        arr[j + 1] = curr # 1 * n -> O(n)
+        arr[j] = curr # 1 * n -> O(n)
 
 
 
